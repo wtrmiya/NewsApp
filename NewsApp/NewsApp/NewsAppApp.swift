8484beb12ac690b084f9211610eb86eac1425c21
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-05 08:19:01 +0900

    chore: #1 Xcodeプロジェクトを作成した

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Wataru Miyakoshi on 2024/06/05.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
