8bf952981e711d1a62864ae374d18627fe8eb43f
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-05 08:24:48 +0900

    chore: #1 SwiftLintおよびAppDelegateを設定した close #1

---
+++ NewsApp/NewsApp/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  NewsApp
+//
+//  Created by Wataru Miyakoshi on 2024/06/05.
+//
+
+import SwiftUI
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        return true
+    }
+}
