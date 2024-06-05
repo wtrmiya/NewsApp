debbdd7d03ae0cbab66d45d3e47a41416dec50b5
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-06 05:40:42 +0900

    function: #9 SignUpViewのUIを配置した

--- NewsApp/NewsApp/Views/SignUp/SignUpView.swift
+++ NewsApp/NewsApp/Views/SignUp/SignUpView.swift
@@ -8,8 +8,63 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var displayName: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmingPassword: String = ""
+
     var body: some View {
-        Text("SignUpView")
+        ZStack(alignment: .top) {
+            Text("Sign Up")
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                    }, label: {
+                        Text("Cancel")
+                    })
+                }
+
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("Display Name")
+                        TextField("Input display name", text: $displayName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Email")
+                        TextField("Input email", text: $displayName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Password")
+                        TextField("Input password", text: $displayName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Confirm Password")
+                        TextField("Confirm password", text: $displayName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+
+                Text("全ての項目に情報を入力してください")
+
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                    }, label: {
+                        Text("Create User")
+                    })
+                    Spacer()
+                }
+
+                Spacer()
+            }
+        }
+        .padding()
     }
 }
 
