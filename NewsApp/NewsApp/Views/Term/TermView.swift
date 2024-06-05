8a80b0827d07aa0cdbcb0a08031bc4715125d878
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-05 19:39:34 +0900

    function: #9 TermViewのUIを配置した

--- NewsApp/NewsApp/Views/Term/TermView.swift
+++ NewsApp/NewsApp/Views/Term/TermView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct TermView: View {
     var body: some View {
-        Text("TermView")
+        NavigationStack {
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, ")
+                .navigationTitle("Term")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: {
+                            print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                        }, label: {
+                            Text("Dismiss")
+                        })
+                    }
+                }
+        }
     }
 }
 
