f9a2b98aa672bf09a90fdeb52c86b2757b612cfd
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-06 05:43:13 +0900

    function: #9 SignInViewのUIを配置した

--- NewsApp/NewsApp/Views/SignIn/SignInView.swift
+++ NewsApp/NewsApp/Views/SignIn/SignInView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+
     var body: some View {
-        Text("SignInView")
+        ZStack(alignment: .top) {
+            Text("Sign Up")
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                    }, label: {
+                        Text("Cancel")
+                    })
+                }
+
+                VStack {
+                    VStack(alignment: .leading) {
+                        Text("Email")
+                        TextField("Input email", text: $email)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    VStack(alignment: .leading) {
+                        Text("Password")
+                        TextField("Input password", text: $password)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                }
+
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                    }, label: {
+                        Text("Sign In")
+                    })
+                    Spacer()
+                }
+
+                Spacer()
+            }
+        }
+        .padding()
     }
 }
 
