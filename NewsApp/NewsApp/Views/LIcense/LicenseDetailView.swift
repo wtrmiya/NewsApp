6b49a62f0e9b01ada7cda0717e12318e415fcd95
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-06 05:31:18 +0900

    function: #9 LicenseDetailViewのUIを配置した

--- NewsApp/NewsApp/Views/LIcense/LicenseDetailView.swift
+++ NewsApp/NewsApp/Views/LIcense/LicenseDetailView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct LicenseDetailView: View {
     var body: some View {
-        Text("LicenseDetailView")
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                    }, label: {
+                        Text("Dismiss")
+                    })
+                }
+            }
     }
 }
 
