38ef96102f911f812d3a0a95dbd231c48ec50b7d
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-05 10:36:13 +0900

    chore: #3 push時のユニットテスト実行を設定した。動作確認のため、必ずテストは失敗する

---
+++ NewsApp/NewsAppTests/NewsAppTests.swift
@@ -0,0 +1,14 @@
+//
+//  NewsAppTests.swift
+//  NewsAppTests
+//
+//  Created by Wataru Miyakoshi on 2024/06/05.
+//
+
+import XCTest
+
+final class NewsAppTests: XCTestCase {
+    func testExample() throws {
+        XCTFail("test fastlane")
+    }
+}
