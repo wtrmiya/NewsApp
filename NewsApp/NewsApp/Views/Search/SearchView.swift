8e77e48d8fb95dcf9737abf63dcd0b177a9e3d0c
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-05 19:32:52 +0900

    function: #9 SearchViewのUIを配置した

--- NewsApp/NewsApp/Views/Search/SearchView.swift
+++ NewsApp/NewsApp/Views/Search/SearchView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var inputText: String = ""
+    
+    let dummyArticle = ["NHK", "2024-06-05", "記事のタイトル", "記事概要テキスト記事概要テキスト記事概要テキスト記事概要テキスト", "apple.logo"]
+
     var body: some View {
-        Text("SearchView")
+        NavigationStack {
+            VStack {
+                TextField("Input word", text: $inputText)
+                    .padding()
+                List {
+                    ForEach(0..<7, id: \.self) { _ in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(dummyArticle[0])
+                                Text(dummyArticle[2])
+                                Text(dummyArticle[3])
+                            }
+                            VStack {
+                                Text(dummyArticle[0])
+                                Image(systemName: dummyArticle[4])
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 60, height: 60)
+                                Image(systemName: "bookmark.fill")
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Search")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                    }, label: {
+                        Text("Cancel")
+                    })
+                }
+            }
+        }
     }
 }
 
