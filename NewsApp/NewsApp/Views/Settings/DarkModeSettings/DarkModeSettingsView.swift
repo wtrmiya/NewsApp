b4c82582ab1b633f91852f7e7559c92551b36a44
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-06 06:09:18 +0900

    function: #9 DarkModeSettingsViewのUIを配置した

--- NewsApp/NewsApp/Views/Settings/DarkModeSettings/DarkModeSettingsView.swift
+++ NewsApp/NewsApp/Views/Settings/DarkModeSettings/DarkModeSettingsView.swift
@@ -9,10 +9,38 @@ import SwiftUI
 
 struct DarkModeSettingsView: View {
     var body: some View {
-        Text("DarkModeSettingsView")
+        VStack {
+            List {
+                HStack {
+                    Image(systemName: "checkmark.circle.fill")
+                    Text("端末の設定に沿う")
+                }
+                HStack {
+                    Image(systemName: "circle")
+                    Text("常にライトモードを使用する")
+                }
+                HStack {
+                    Image(systemName: "circle")
+                    Text("常にダークモードを使用する")
+                }
+            }
+        }
+        .navigationTitle("ダークモードの設定")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    print("NOT IMPLEMENTED: file: \(#file), line: \(#line)")
+                }, label: {
+                    Text("Dismiss")
+                })
+            }
+        }
     }
 }
 
 #Preview {
-    DarkModeSettingsView()
+    NavigationStack {
+        DarkModeSettingsView()
+    }
 }
