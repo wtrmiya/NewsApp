58b4065895b65c7e23c8c5028fecdecfc4d0046f
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-08 07:17:08 +0900

    chore: #15 SignInエラー時のアラート表示を実装した

--- NewsApp/NewsApp/Views/SignIn/SignInView.swift
+++ NewsApp/NewsApp/Views/SignIn/SignInView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct SignInView: View {
     @StateObject private var signInViewModel: SignInViewModel = SignInViewModel()
+    @State private var isShowingAlert: Bool = false
+    @Environment(\.dismiss) private var dismiss
 
     var body: some View {
         ZStack(alignment: .top) {
@@ -52,6 +54,22 @@ struct SignInView: View {
             }
         }
         .padding()
+        .alert("Error", isPresented: $isShowingAlert, actions: {
+            Button(action: {
+                dismiss()
+            }, label: {
+                Text("OK")
+            })
+        }, message: {
+            if let errorMessage = signInViewModel.errorMessage {
+                Text(errorMessage)
+            }
+        })
+        .onReceive(signInViewModel.$errorMessage, perform: { _ in
+            if signInViewModel.errorMessage != nil {
+                isShowingAlert = true
+            }
+        })
     }
     
     private func signIn() async {
