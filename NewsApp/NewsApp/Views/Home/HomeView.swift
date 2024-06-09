25f6c856687ed6ed1115d086321ccc3ad3024e50
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-10 06:14:39 +0900

    function: #18 HomeViewで記事をタップして元URLへ遷移させる処理を実装した close #18

--- NewsApp/NewsApp/Views/Home/HomeView.swift
+++ NewsApp/NewsApp/Views/Home/HomeView.swift
@@ -18,7 +18,14 @@ struct HomeView: View {
         "科学"
     ]
     
-    let dummyArticle = ["NHK", "2024-06-05", "記事のタイトル", "記事概要テキスト記事概要テキスト記事概要テキスト記事概要テキスト", "apple.logo"]
+    let dummyArticle = [
+        "NHK",
+        "2024-06-05",
+        "記事のタイトル",
+        "記事概要テキスト記事概要テキスト記事概要テキスト記事概要テキスト",
+        "apple.logo",
+        "https://apple.com"
+    ]
     
     @State private var isShowingSearchView: Bool = false
     @State private var isShowingDrawer: Bool = false
@@ -41,19 +48,21 @@ struct HomeView: View {
                     }
                     List {
                         ForEach(0..<7, id: \.self) { _ in
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Text(dummyArticle[0])
-                                    Text(dummyArticle[2])
-                                    Text(dummyArticle[3])
-                                }
-                                VStack {
-                                    Text(dummyArticle[0])
-                                    Image(systemName: dummyArticle[4])
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 60, height: 60)
-                                    Image(systemName: "bookmark.fill")
+                            Link(destination: URL(string: dummyArticle[5])!) {
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(dummyArticle[0])
+                                        Text(dummyArticle[2])
+                                        Text(dummyArticle[3])
+                                    }
+                                    VStack {
+                                        Text(dummyArticle[0])
+                                        Image(systemName: dummyArticle[4])
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 60, height: 60)
+                                        Image(systemName: "bookmark.fill")
+                                    }
                                 }
                             }
                         }
