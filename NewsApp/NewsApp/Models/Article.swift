dcc43d63ca19e1b13aacbb31dab5282696e44cf7
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-12 04:26:29 +0900

    chore: #38 ArticleにFirestore管理用のプロパティを追加した

--- NewsApp/NewsApp/Models/Article.swift
+++ NewsApp/NewsApp/Models/Article.swift
@@ -43,6 +43,52 @@ struct Article: Decodable, Hashable {
     let url: String
     let urlToImage: String?
     let publishedAt: String
+    let bookmarked: Bool
+    let documentId: String?
+    let bookmarkedAt: Date?
+    
+    enum CodingKeys: String, CodingKey {
+        case source, author, title, description, url, urlToImage, publishedAt
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.source = try container.decode(ArticleSource.self, forKey: .source)
+        self.author = try container.decodeIfPresent(String.self, forKey: .author)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.description = try container.decodeIfPresent(String.self, forKey: .description)
+        self.url = try container.decode(String.self, forKey: .url)
+        self.urlToImage = try container.decodeIfPresent(String.self, forKey: .urlToImage)
+        self.publishedAt = try container.decode(String.self, forKey: .publishedAt)
+        
+        self.bookmarked = false
+        self.documentId = nil
+        self.bookmarkedAt = nil
+    }
+    
+    init(
+        source: ArticleSource,
+        author: String?,
+        title: String,
+        description: String?,
+        url: String,
+        urlToImage: String?,
+        publishedAt: String,
+        bookmarked: Bool,
+        documentId: String?,
+        bookmarkedAt: Date?
+    ) {
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.bookmarked = bookmarked
+        self.documentId = documentId
+        self.bookmarkedAt = bookmarkedAt
+    }
 }
 
 struct ArticleSource: Decodable, Hashable {
