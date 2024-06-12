bfe814b301c3549039411a4ebb8d86aaff8f22b1
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-12 16:44:36 +0900

    function: #40 ブックマーク画面表示時にFirestoreからブックマーク情報を取得するようにした

--- NewsApp/NewsApp/Views/Bookmark/BookmarkViewModel.swift
+++ NewsApp/NewsApp/Views/Bookmark/BookmarkViewModel.swift
@@ -8,32 +8,7 @@
 import Foundation
 
 final class BookmarkViewModel: ObservableObject {
-    @Published var articles: [Article] = [
-        Article(
-            source: ArticleSource(name: "Example.com"),
-            author: nil,
-            title: "サンプル記事 タイトル 先発出場で二塁打もチーム敗れる | Example | サンプル記事 - example.com",
-            description: "サンプル記事、ドジャースの選手は9日、ヤンキースとの3連戦の最終戦に先発出場し、ツーベースヒット1本を打ちましたが、…",
-            url: "https://www.example.com/news/html/20240610/k10014475991000.html",
-            urlToImage: "https://www.example.com/news/html/20240610/sample_image_01.jpg",
-            publishedAt: "2024-06-10T04:12:02Z",
-            bookmarked: false,
-            documentId: nil,
-            bookmarkedAt: nil
-        ),
-        Article(
-            source: ArticleSource(name: "Example.com"),
-            author: nil,
-            title: "サンプル記事 タイトル 先発出場で二塁打もチーム敗れる | Example | サンプル記事 - example.com",
-            description: "サンプル記事、ドジャースの選手は9日、ヤンキースとの3連戦の最終戦に先発出場し、ツーベースヒット1本を打ちましたが、…",
-            url: "https://www.example.com/news/html/20240610/k10014475991000.html",
-            urlToImage: "https://www.example.com/news/html/20240610/sample_image_01.jpg",
-            publishedAt: "2024-06-10T04:12:02Z",
-            bookmarked: false,
-            documentId: nil,
-            bookmarkedAt: nil
-        )
-    ]
+    @Published var articles: [Article] = []
     
     @Published var errorMessage: String?
     
@@ -48,10 +23,14 @@ final class BookmarkViewModel: ObservableObject {
         accountManager.isSignedIn
     }
     
+    @MainActor
     func populateBookmarkedArticles() async {
         do {
-//            let articles = try await bookmarkManager.getBookmarkedArticles()
-//            self.articles = articles
+            guard let currentUser = accountManager.user else {
+                return
+            }
+            let bookmarkedArticles = try await bookmarkManager.getBookmarks(uid: currentUser.uid)
+            self.articles = bookmarkedArticles
         } catch {
             if let error = error as? NetworkError {
                 self.errorMessage = error.rawValue
