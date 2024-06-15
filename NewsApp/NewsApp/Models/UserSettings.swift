fdd1b2b9d9c86e47446dea875a4324f593e822ae
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-15 13:48:08 +0900

    chore: #49 UserSettingsに文字設定を追加した

--- NewsApp/NewsApp/Models/UserSettings.swift
+++ NewsApp/NewsApp/Models/UserSettings.swift
@@ -13,6 +13,8 @@ struct UserSettings {
     var pushMorningEnabled: Bool
     var pushAfternoonEnabled: Bool
     var pushEveningEnabled: Bool
+    var letterSize: Int
+    var letterWeight: Int
     let createdAt: Date
     var updatedAt: Date
 }
@@ -24,6 +26,8 @@ extension UserSettings {
             pushMorningEnabled: true,
             pushAfternoonEnabled: true,
             pushEveningEnabled: true,
+            letterSize: 1,
+            letterWeight: 0,
             createdAt: Date(),
             updatedAt: Date()
         )
@@ -35,6 +39,8 @@ extension UserSettings {
             pushMorningEnabled: true,
             pushAfternoonEnabled: true,
             pushEveningEnabled: true,
+            letterSize: 1,
+            letterWeight: 0,
             createdAt: Date(),
             updatedAt: Date()
         )
@@ -54,6 +60,8 @@ extension UserSettings {
             "push_morning_enabled": pushMorningEnabled,
             "push_afternoon_enabled": pushAfternoonEnabled,
             "push_evening_enabled": pushEveningEnabled,
+            "letter_size": letterSize,
+            "letter_weight": letterWeight,
             "created_at": createdAt,
             "updated_at": updatedAt
         ]
@@ -66,6 +74,8 @@ extension UserSettings {
             let pushMorningEnabled = dictionary["push_morning_enabled"] as? Bool,
             let pushAfternoonEnabled = dictionary["push_afternoon_enabled"] as? Bool,
             let pushEveningEnabled = dictionary["push_evening_enabled"] as? Bool,
+            let letterSize = dictionary["letter_size"] as? Int,
+            let letterWeight = dictionary["letter_weight"] as? Int,
             let createdAt = (dictionary["created_at"] as? Timestamp)?.dateValue(),
             let updatedAt = (dictionary["updated_at"] as? Timestamp)?.dateValue()
         else {
@@ -77,6 +87,8 @@ extension UserSettings {
             pushMorningEnabled: pushMorningEnabled,
             pushAfternoonEnabled: pushAfternoonEnabled,
             pushEveningEnabled: pushEveningEnabled,
+            letterSize: letterSize,
+            letterWeight: letterWeight,
             createdAt: createdAt,
             updatedAt: updatedAt
         )
