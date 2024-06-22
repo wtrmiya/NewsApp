d3a6afab232b014aa28ca73c95e192ce4cf6eee6
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-06-23 06:09:45 +0900

    chore: #80 トーストの表示方法を調整した

--- NewsApp/NewsApp/Views/Toast/ToastView.swift
+++ NewsApp/NewsApp/Views/Toast/ToastView.swift
@@ -18,12 +18,12 @@ struct ToastView: View {
         Group {
             if let toastMessage = toastHandler.currentToastMessage {
                 Text(toastMessage)
-                    .frame(width: 250, height: 50)
-                    .font(.headline)
+                    .frame(width: 300, height: 50)
+                    .font(.footnote)
                     .foregroundStyle(.white)
                     .background(Color.green)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .transition(.move(edge: .bottom))
+                    .transition(.toastMove.combined(with: .opacity))
             }
         }
         .animation(.easeInOut, value: toastHandler.currentToastMessage)
@@ -39,6 +39,24 @@ extension View {
             on: .bottom,
             handledBy: toastHandler,
             toastMaker: { ToastView(toastHandler: $0) }
+        )
+    }
+}
+
+fileprivate extension AnyTransition {
+    private struct YOffsetModifier: ViewModifier {
+        let yOffset: CGFloat
+        
+        func body(content: Content) -> some View {
+            content
+                .offset(y: yOffset)
+        }
+    }
+    
+    static var toastMove: AnyTransition {
+        AnyTransition.modifier(
+            active: YOffsetModifier(yOffset: 0),
+            identity: YOffsetModifier(yOffset: -70)
         )
     }
 }
