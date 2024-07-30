435dd0eea78523668b7dc040998e56d98072cc37
Wataru Miyakoshi <wtrmiya@gmail.com>
2024-07-31 06:44:20 +0900

    chore: #132 UserDataStoreManagerのエラーを整理した

--- NewsApp/NewsApp/Managers/UserDataStore/UserDataStoreManager.swift
+++ NewsApp/NewsApp/Managers/UserDataStore/UserDataStoreManager.swift
@@ -9,6 +9,12 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
+enum UserDataStoreManagerError: Error {
+    case rejectedWritingDocument
+    case failedFetchingUserDataStoreDocuments
+    case documentDoesNotExist
+}
+
 final class UserDataStoreManager {
     static let shared = UserDataStoreManager()
     private init() {}
@@ -24,19 +30,32 @@ private extension UserDataStoreManager {
 
 extension UserDataStoreManager: UserDataStoreManagerProtocol {
     func createUserDataStore(userAccount: UserAccount) async throws {
-        try await usersCollectionRef.addDocument(data: [
-            "uid": userAccount.uid,
-            "displayName": userAccount.displayName
-        ])
+        do {
+            try await usersCollectionRef.addDocument(data: [
+                "uid": userAccount.uid,
+                "displayName": userAccount.displayName
+            ])
+        } catch {
+            print(error)
+            throw UserDataStoreManagerError.rejectedWritingDocument
+        }
     }
     
     func getUserDataStoreDocumentId(userAccount: UserAccount) async throws -> String {
-        guard let userDocumentId = try await usersCollectionRef
+        let documents: [QueryDocumentSnapshot]
+        do {
+            documents = try await usersCollectionRef
             .whereField("uid", isEqualTo: userAccount.uid)
             .getDocuments()
-            .documents.first?.documentID
+            .documents
+        } catch {
+            print(error)
+            throw UserDataStoreManagerError.failedFetchingUserDataStoreDocuments
+        }
+
+        guard let userDocumentId = documents.first?.documentID
         else {
-            throw NetworkError.invalidData
+            throw UserDataStoreManagerError.documentDoesNotExist
         }
         
         return userDocumentId
